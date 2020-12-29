bc933d2f847106bb10ecf4847230eec09a6af143
nwkwok <nwkwok@gmail.com>
2020-12-28 19:25:27 -0800

    create-react-app, setup node-sass and variables.scss

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './App.module.scss'
+
+function App() {
+  return (
+    <div className={style.container}>
+      Hello world!
+    </div>
+  )
+}
+
+export default App
+
+
