45348deac3d498a1407dff192c9e0b270216c8c5
nwkwok <nwkwok@gmail.com>
2021-01-01 22:34:37 -0800

    import react-circular-progressbar and animate percentage state

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from './App.module.scss'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css'
+
+
 
 function App() {
+  
+  const [percentage, setPercentage] = useState(80)
+
+  const handleChange = () => {
+    setPercentage(prevPercentage => {
+      console.log(prevPercentage);
+      return prevPercentage + 1
+    })
+
+  }
   return (
     <div className={style.container}>
-      Hello world!
+      <h1>pomodoro</h1>
+      <p>pomodoro   short break   long break</p>
+      <CircularProgressbar
+        className={style.progressBar}
+        value={percentage === 100 ? setPercentage(0) : percentage}
+        text={`${percentage}`}
+        strokeWidth={3}
+        styles={buildStyles({
+          pathColor: `rgba(255, 255, 255, ${percentage/100})`,
+          trailColor: 'red',
+        })}
+        />
+      <p>Cog Image</p>
+      <button onClick={handleChange}>Change #</button>
     </div>
   )
 }
