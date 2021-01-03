1de700ff701e0f37663975403c3e6026f0813667
nwkwok <nwkwok@gmail.com>
2021-01-03 12:54:35 -0800

    brainstorm thru build process

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,49 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import style from './App.module.scss'
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css'
 
-
-
 function App() {
+  const [time, setTime] = useState(15)
+  const [second, setSecond] = useState(0)
+
   
-  const [percentage, setPercentage] = useState(80)
+//Todo #1: 
+  // Get current date
+  // Get a then date to set time difference 
+  // Create function that takes in a time value 
+  // Create setInterval in useEffect
+  // Use setTime to countdown the state of time every second
+  
+  /////// Configuring timer ///////
+  // const now = new Date();
+  // const getTime = now.getTime();
+  // const hours = now.getHours();
+  // const minutes = now.getMinutes();
+  // const seconds = now.getSeconds();
+  // console.log({now, hours, minutes, seconds, getTime})
+
+// Todo #2:
+  // Create data-time attribute for 'Pomodoro' 'SB' 'LB'
+  // Set data-time to the # of minutes 
+  // Have onClick of each element setTime to its data-time value
+  
+// Todo #3
+  // Design and create modal using usePortal
+  // Need to decide between props and useContext.
+  // Probably props since it's only one component.
+  // Configure state management for modal
+
+// Todo #4
+  // Work on React Circular Progress Bar functionality
+  // Figure out how to progress bar every second
+
+
 
   const handleChange = () => {
-    setPercentage(prevPercentage => {
-      console.log(prevPercentage);
-      return prevPercentage + 1
+    setTime(prevTimer => {
+      console.log(prevTimer);
+      return prevTimer + 1
     })
 
   }
@@ -22,13 +53,9 @@ function App() {
       <p>pomodoro   short break   long break</p>
       <CircularProgressbar
         className={style.progressBar}
-        value={percentage === 100 ? setPercentage(0) : percentage}
-        text={`${percentage}`}
-        strokeWidth={3}
-        styles={buildStyles({
-          pathColor: `rgba(255, 255, 255, ${percentage/100})`,
-          trailColor: 'red',
-        })}
+        value={time}
+        text={`${time}`}
+        strokeWidth={2}
         />
       <p>Cog Image</p>
       <button onClick={handleChange}>Change #</button>
